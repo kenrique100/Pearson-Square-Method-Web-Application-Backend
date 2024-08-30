941e2854964a75208fb2b9d645ca5c0b81af08c9
Kenrique <ngwakneri2016@gmail.com>
2024-08-30 12:20:13 +0100

    java backend config

---
+++ src/main/java/com/api/feedFormulation/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.api.feedFormulation.config;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NotNull CorsRegistry registry) {
+                registry.addMapping("/api/**") // Adjust this to match your API endpoint patterns
+                        .allowedOrigins("http://localhost:3000") // Frontend's origin
+                        .allowedMethods("GET", "POST", "PUT", "DELETE") // Allowed HTTP methods
+                        .allowedHeaders("*") // Allowed headers
+                        .allowCredentials(true); // Allow cookies
+            }
+        };
+    }
+}
