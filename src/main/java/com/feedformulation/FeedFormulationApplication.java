9d062bd62f0e8514f087b4bb7c573aadcedc61c4
Kenrique _Ngwa <ngwakenri2016@gmail.com>
2024-09-08 23:07:20 +0100

    main app

---
+++ src/main/java/com/feedformulation/FeedFormulationApplication.java
@@ -0,0 +1,12 @@
+package com.feedformulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedFormulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedFormulationApplication.class, args);
+	}
+}
