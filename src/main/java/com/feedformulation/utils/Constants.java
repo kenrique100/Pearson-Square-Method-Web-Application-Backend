085d1f249bfd07d8b1f79c721fb90f59baa8c3ff
Kenrique _Ngwa <ngwakenri2016@gmail.com>
2024-09-08 23:03:52 +0100

    Ingredient constants

--- src/main/java/com/feedformulation/utils/Constants.java
+++ src/main/java/com/feedformulation/utils/Constants.java
@@ -1,4 +1,4 @@
-package com.api.feedFormulation.utils;
+package com.feedformulation.utils;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
