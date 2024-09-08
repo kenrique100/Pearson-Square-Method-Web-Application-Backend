11af068ffd58226d18fe8d8f044b2b182ca58334
Kenrique _Ngwa <ngwakenri2016@gmail.com>
2024-09-08 23:00:59 +0100

    Exception handlers

--- src/main/java/com/api/feedFormulation/exception/InvalidInputException.java
+++
@@ -1,7 +0,0 @@
-package com.api.feedFormulation.exception;
-
-public class InvalidInputException extends RuntimeException {
-    public InvalidInputException(String message) {
-        super(message);
-    }
-}
