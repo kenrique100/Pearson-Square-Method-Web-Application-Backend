870101c1c779cfbd6da6b5402efbf6e2d4c317dd
Kenrique _Ngwa <ngwakenri2016@gmail.com>
2024-09-13 00:42:30 +0100

    updated res mapping to remove id and feedFormulation

--- src/main/java/com/feedformulation/service/FeedFormulationService2.java
+++ src/main/java/com/feedformulation/service/FeedFormulationService2.java
@@ -12,39 +12,42 @@ public interface FeedFormulationService2 {
      * Creates a new feed formulation based on the provided request.
      *
      * @param request The request containing the data needed to create the feed formulation
-     * @return The newly created CustomFeedFormulation entity
+     * @return The newly created FeedFormulation entity
      */
     FeedFormulation createFeedFormulation(FeedFormulationRequest request);
 
     /**
-     * Retrieves a feed formulation by its unique ID.
+     * Retrieves a feed formulation by its unique ID and date.
      *
-     * @param id The unique identifier of the feed formulation
-     * @return The CustomFeedFormulation entity corresponding to the provided ID
+     * @param id   The unique identifier of the feed formulation
+     * @param date The date of the formulation.
+     * @return The FeedFormulation entity corresponding to the provided ID and date
      */
-    FeedFormulation getFeedFormulationById(String id);
+    FeedFormulation getFeedFormulationByIdAndDate(String id, String date);
 
     /**
      * Retrieves a list of all feed formulations.
      *
-     * @return A list of all CustomFeedFormulation entities
+     * @return A list of all FeedFormulationResponse entities
      */
     List<FeedFormulationResponse> getAllFeedFormulations();
 
     /**
-     * Updates an existing feed formulation with the provided data.
+     * Updates an existing feed formulation with the provided data, identified by ID and date.
      *
-     * @param id The unique identifier of the feed formulation to be updated
+     * @param id      The unique identifier of the feed formulation to be updated
+     * @param date    The date of the formulation to be updated
      * @param request The request containing the updated data for the feed formulation
-     * @return The updated CustomFeedFormulation entity
+     * @return The updated FeedFormulation entity
      */
-    FeedFormulation updateFeedFormulation(String id, FeedFormulationRequest request);
+    FeedFormulation updateFeedFormulationByIdAndDate(String id, String date, FeedFormulationRequest request);
 
     /**
-     * Deletes a feed formulation by its unique ID.
+     * Deletes a feed formulation by its unique ID and date.
      *
-     * @param id The unique identifier of the feed formulation to be deleted
+     * @param id   The unique identifier of the feed formulation to be deleted
+     * @param date The date of the formulation to be deleted
      */
-    void deleteFeedFormulation(String id);
+    void deleteFeedFormulationByIdAndDate(String id, String date);
 
 }
