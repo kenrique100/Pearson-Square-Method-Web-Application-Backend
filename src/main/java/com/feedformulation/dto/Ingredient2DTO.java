4e3e609d7a8ee25dfe797eb119e7224d2fcbc77f
Kenrique _Ngwa <ngwakenri2016@gmail.com>
2024-09-13 00:42:50 +0100

    updated objects

--- src/main/java/com/feedformulation/dto/Ingredient2DTO.java
+++ src/main/java/com/feedformulation/dto/Ingredient2DTO.java
@@ -6,10 +6,8 @@ import lombok.Data;
 @Data
 @Builder
 public class Ingredient2DTO {
-    private Long id;
     private String name;
     private double crudeProtein;
     private double quantityKg;
-    private String feedFormulation; // Include formulation ID
 }
 
