5f163ee49eea441217f50b617d434e2648d87518
Kenrique _Ngwa <ngwakenri2016@gmail.com>
2024-09-21 10:07:58 +0100

    App props updated

--- src/main/java/com/feedformulation/config/WebConfig.java
+++ src/main/java/com/feedformulation/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.feedformulation.config;
 
 import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,6 +9,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig {
+    @Value("${frontend.url}")  // Fixed typo by adding the closing curly brace
+    private String frontendUrl;
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -16,14 +19,14 @@ public class WebConfig {
             public void addCorsMappings(@NotNull CorsRegistry registry) {
                 // Configure CORS for /api/feed-formulations endpoints
                 registry.addMapping("/api/feed-formulations/**")
-                        .allowedOrigins("https://pearson-square-method-web-application-frontend.vercel.app/") // Frontend origin
+                        .allowedOrigins(frontendUrl) // Frontend origin
                         .allowedMethods("GET", "POST", "PUT", "DELETE") // Allowed HTTP methods
                         .allowedHeaders("*") // Allowed headers
                         .allowCredentials(true); // Allow credentials (e.g., cookies)
 
                 // Configure CORS for /v1/api/feed-formulation endpoints
                 registry.addMapping("/api/feed-formulation/**")
-                        .allowedOrigins("https://pearson-square-method-web-application-frontend.vercel.app/") // Frontend origin
+                        .allowedOrigins(frontendUrl) // Frontend origin
                         .allowedMethods("GET", "POST", "PUT", "DELETE") // Allowed HTTP methods
                         .allowedHeaders("*") // Allowed headers
                         .allowCredentials(true); // Allow credentials (e.g., cookies)
