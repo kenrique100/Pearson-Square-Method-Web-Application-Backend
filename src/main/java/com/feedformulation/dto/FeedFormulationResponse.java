1cfc9976356d81d6019d71f198035ba077eeb949
Kenrique _Ngwa <ngwakenri2016@gmail.com>
2024-09-25 07:33:03 +0100

    response for default feed calculation

--- src/main/java/com/feedformulation/dto/FeedFormulationResponse.java
+++ src/main/java/com/feedformulation/dto/FeedFormulationResponse.java
@@ -9,10 +9,22 @@ import java.util.List;
 @Data
 @Builder
 public class FeedFormulationResponse {
+
+    // Unique identifier for the feed formulation
     private String formulationId;
+
+    // Name of the feed formulation
     private String formulationName;
+
+    // Date when the formulation was created
     private LocalDate date;
+
+    // Total quantity of the feed formulation in kilograms
     private double totalQuantityKg;
+
+    // Target crude protein (CP) value for the formulation
     private double targetCpValue;
+
+    // List of ingredients used in the feed formulation
     private List<Ingredient2DTO> ingredient2s;
 }
