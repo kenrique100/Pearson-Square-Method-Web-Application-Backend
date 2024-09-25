8ffcf63ef5783e66e3df3ce22638d3b840f4d030
Kenrique _Ngwa <ngwakenri2016@gmail.com>
2024-09-25 07:32:32 +0100

    added json ignore

--- src/main/java/com/feedformulation/dto/FeedFormulationRequest.java
+++ src/main/java/com/feedformulation/dto/FeedFormulationRequest.java
@@ -9,19 +9,25 @@ import java.util.List;
 @Data
 public class FeedFormulationRequest {
 
+    // Field to capture the name of the feed formulation, cannot be null
     @NotNull(message = "Formulation name is required.")
     private String formulationName;
 
+    // List of protein ingredients required in the formulation, cannot be empty
     @NotEmpty(message = "At least one protein ingredient is required.")
     private List<IngredientRequest> proteins;  // List for protein ingredients
 
+    // List of carbohydrate ingredients required in the formulation, cannot be empty
     @NotEmpty(message = "At least one carbohydrate ingredient is required.")
     private List<IngredientRequest> carbohydrates;  // List for carbohydrate ingredients
 
     @Data
     public static class IngredientRequest {
+        // Name of the ingredient, cannot be null
         @NotNull
         private String name;
+
+        // Quantity of the ingredient in kilograms, cannot be null
         @NotNull
         private double quantityKg;
     }
